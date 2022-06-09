acd113c86a8b833ecd9d4c2e0a6f3f7d36e8a23e
@PittsPreneur <60836218+pittspreneur@users.noreply.github.com>
2022-06-09 00:33:57 -0500

    Add files via upload

---
+++ Intro To Python.py
@@ -0,0 +1,274 @@
+'''
+Python Basics with @Pittspreneur
+
+
+Thank you for your support! If you have any questions,
+reach out to me on social media @Pittspreneur!
+
+'''
+
+#================================
+print()
+print("Section 1.0 - Intro To Python")
+#Section 1.0 - Hello World
+'''
+Here is an intro to python! I hope you learn and start coding.
+'''
+
+
+
+#================================
+print()
+print("Section 1.1 - Hello World")
+#Section 1.1 - Hello World
+'''
+Printing is outputting a message to the terminal where a
+user can see.
+
+The code below prints Hello World! to the terminal.
+'''
+
+print("Hello World!")
+
+
+
+
+#================================
+print()
+print("Section 1.2 - Comments")
+#Section 1.2 - Comments
+'''
+Comments allow developers to store notes within their code for
+themselves or other developers to see.
+'''
+
+
+
+
+#================================
+print()
+print("Section 1.3 - Strings")
+#Section 1.3 - Strings
+'''
+Strings are blocks of text. In 1.1, we printed a string that printed
+out 'Hello World'.
+
+Strings can use single or double quotation marks, just make sure that
+they correspond
+'''
+
+name = "John"
+
+day = 'saturday'
+
+
+
+
+#================================
+print()
+print("Section 1.4 - Variables")
+#Section 1.4 - Variables
+'''
+Variables are a way to store data to be resued while coding. Variables
+can store different types of information and they can be updated throughout
+your code.
+
+To create a variable, you need to name it and then use '=' next to it's
+value.
+
+In Python, it's common practice to use 'snake case'. Snake case means
+placing a '_' inbetween each word instead of a space.
+'''
+
+my_meal = "Fruit & Water"
+
+print("Breakfast...")
+print(my_meal)
+
+my_meal = "Shrimp Fried Rice"
+
+print("Lunch...")
+print(my_meal)
+
+my_meal = "Steak & Potatoes"
+
+print("Dinner")
+print(my_meal)
+
+
+
+#================================
+print()
+print("Section 1.5 - Errors")
+#Section 1.5 - Errors
+'''
+I have never ran a program I was creating and it contained no errors. Even
+as something as simple as a comma can stop your program from working.
+
+One of the most common errors are Syntax Errors. this occirs when the
+puncuation of your code contains an error.
+'''
+
+
+
+
+#================================
+print()
+print("Section 1.6 - Numbers")
+#Section 1.6 - Numbers
+'''
+This shouldnt be a suprise, of course there are numbers in coding! However,
+there are two types dedicated to numbers - floats and intergers. An integer
+are numbers like 1, 2000, and 1000000000. Floats on the other hand are decimal
+numbers.
+
+This (1000000000) number is hard to read but there is a solution - underscores.
+They print the same number with or without and look like... 1_000_000_000.
+'''
+
+coin_name = 'Bitcoin'
+coin_price = 50_000
+coin_number = 21_000_000
+
+
+my_wallet = 1
+my_balance = my_wallet * coin_price
+
+print(my_balance)
+
+
+
+
+#================================
+print()
+print("Section 2.0 - Control Flow")
+#Section 1.1 - Hello World
+'''
+Control Flow is the process of making decisons through yes and no arguments. For
+example, lets say you are about to go out. If it was Sunday, you may say you need to
+your suit for church. If it was Monday, you may say you need to go to work or school
+and grab your bag on your way out and check for nay assignemnts. If it was saturday
+or a holida, you may not need to do anything when you wake up.
+
+This is an example of a control flow. Another example is order of operations.
+'''
+
+
+#================================
+print()
+print("Section 2.1 - Boolean Expressions")
+#Section 2.1 - Boolean Expressions
+'''
+Boolean expressions refer to true and false statements. It is important to make sure that
+boolean statements refer to a statement and not an opinion.
+'''
+
+
+#================================
+print()
+print("Section 2.2 - Relational Operations: Equals and Not Equals")
+#Section 2.2 - Relational Operations: Equals and Not Equals
+'''
+Printing is outputting a message to the terminal where a
+user can see.
+
+The code below prints Hello World! to the terminal.
+'''
+
+print("Hello World!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

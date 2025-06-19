da1e12d0a9e3caeaf4d581853e10401aa68bbfe3
@PittsPreneur <60836218+pittspreneur@users.noreply.github.com>
2025-06-19 14:21:39 -0500

    Update and rename Hello Word to lesson_intro_variables_and_strings.py
    This script demonstrates how to assign and manipulate variables in Python. It shows how to store numeric and string data, perform arithmetic operations, and build a greeting message using string concatenation. It's a great starting point for beginners learning how to print values and construct dynamic messages using variables.

---
+++ lesson_variables_and_strings.py
@@ -0,0 +1,9 @@
+# This script demonstrates variable assignment, string concatenation, and printing output in Python.
+# It introduces basic syntax for working with numbers and text.
+
+my_age = 28
+half_my_age = 28 / 2
+greeting = 'Hello my name is '
+name = 'John'
+greeting_with_name = greeting + name
+print(greeting_with_name)
